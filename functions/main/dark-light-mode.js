fa1d7d8c7c5580773dcca8a8e1c0b23470fa1999
Personalism <edwinhermili@gmail.com>
2023-12-30 12:41:41 +0700

    index.html Completed

--- functions/main/dark-light-mode.js
+++ functions/main/dark-light-mode.js
@@ -4,16 +4,22 @@ document.addEventListener("DOMContentLoaded", function () {
   var navBar = document.getElementById("navigation-bar");
   var body = document.getElementById("bodies");
   var progressBar = document.getElementById("progress-bar");
+  var dropdown = document.getElementById("dropdown");
+  var thirdSection = document.getElementById("third-section");
 
   checkbox.addEventListener("change", function () {
     if (checkbox.checked) {
       navBar.classList.add("dark-mode");
       body.classList.add("dark-mode");
       progressBar.classList.add("dark-mode");
+      dropdown.classList.add("dark-mode");
+      thirdSection.classList.add("dark-mode");
     } else {
       navBar.classList.remove("dark-mode");
       body.classList.remove("dark-mode");
       progressBar.classList.remove("dark-mode");
+      dropdown.classList.remove("dark-mode");
+      thirdSection.classList.remove("dark-mode");
     }
   });
 });
