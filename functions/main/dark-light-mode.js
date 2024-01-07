ccda06a0bf9a1f341385e1df0fbca6ceb2db2a6e
Personalism <edwinhermili@gmail.com>
2024-01-07 11:52:00 +0700

    Updated

--- functions/main/dark-light-mode.js
+++ functions/main/dark-light-mode.js
@@ -1,25 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var checkbox = document.getElementById("switch");
+  const themeSwitch = document.getElementById("switch");
 
-  var navBar = document.getElementById("navigation-bar");
-  var body = document.getElementById("bodies");
-  var progressBar = document.getElementById("progress-bar");
-  var dropdown = document.getElementById("dropdown");
-  var thirdSection = document.getElementById("third-section");
+  // Variables
+  let navBar = document.getElementById("navigation-bar");
+  let body = document.getElementById("bodies");
+  let progressBar = document.getElementById("progress-bar");
+  let dropdown = document.getElementById("dropdown");
+  let thirdSection = document.getElementById("third-section");
 
-  checkbox.addEventListener("change", function () {
-    if (checkbox.checked) {
+  // Check the user's preference in localStorage
+  const currentTheme = localStorage.getItem("theme");
+  if (currentTheme) {
+    themeSwitch.checked = currentTheme === "dark-mode";
+  }
+
+  // Toggle theme on checkbox change
+  themeSwitch.addEventListener("change", function () {
+    if (themeSwitch.checked) {
+      //
       navBar.classList.add("dark-mode");
       body.classList.add("dark-mode");
       progressBar.classList.add("dark-mode");
       dropdown.classList.add("dark-mode");
       thirdSection.classList.add("dark-mode");
+      //
+      localStorage.setItem("theme", "dark-mode");
     } else {
+      //
       navBar.classList.remove("dark-mode");
       body.classList.remove("dark-mode");
       progressBar.classList.remove("dark-mode");
       dropdown.classList.remove("dark-mode");
       thirdSection.classList.remove("dark-mode");
+      //
+      localStorage.setItem("theme", "light-mode");
     }
   });
+  navBar.classList.add(currentTheme);
+  body.classList.add(currentTheme);
+  progressBar.classList.add(currentTheme);
+  dropdown.classList.add(currentTheme);
+  thirdSection.classList.add(currentTheme);
 });
