dd8b02b06616ba1aec0bd98cbd0ac178c5ef6cbb
Personalism <edwinhermili@gmail.com>
2024-01-08 17:02:31 +0700

    Updated

--- functions/main/footer.js
+++ functions/main/footer.js
@@ -1,16 +1,53 @@
 // footer.js
 
-export function handleFooterScroll() {
-  var footers = document.querySelectorAll("footer");
+// export function handleFooterScroll() {
+//   var footers = document.querySelectorAll("footer");
 
-  footers.forEach((foo) => {
-    var Ftop = window.scrollY;
-    var Foffset = foo.offsetTop - 900;
-    var Fheight = foo.offsetHeight;
-    if (Ftop >= Foffset && Ftop < Foffset + Fheight) {
-      foo.classList.add("show-footer");
-    } else {
-      foo.classList.remove("show-footer");
-    }
-  });
+//   footers.forEach((foo) => {
+//     var Ftop = window.scrollY;
+//     var Foffset = foo.offsetTop + 700;
+//     var Fheight = foo.offsetHeight;
+//     if (Ftop >= Foffset && Ftop < Foffset + Fheight) {
+//       foo.classList.add("show-footer");
+//     } else {
+//       foo.classList.remove("show-footer");
+//     }
+//   });
+// }
+
+export function handleFooterScroll() {
+  var footer = document.getElementById("footer");
+
+  var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+  // Adjust this value as needed based on when you want the div to appear
+  var triggerPosition = document.body.offsetHeight - 200 - window.innerHeight;
+
+  if (scrollPosition >= triggerPosition) {
+    footer.classList.add("show-footer");
+  } else {
+    footer.classList.remove("show-footer");
+  }
 }
+
+// >>>
+// document.addEventListener("DOMContentLoaded", function () {
+//   var footer = document.getElementById("footer");
+//   var divE = document.getElementById("tester");
+
+//   window.onscroll = function () {
+//     var scrollPosition =
+//       window.scrollY || document.documentElement.scrollTop;
+
+//     // Adjust this value as needed based on when you want the div to appear
+//     var triggerPosition = document.body.offsetHeight - 500 - window.innerHeight;
+
+//     if (scrollPosition >= triggerPosition) {
+//       footer.classList.add("show-footer");
+//       divE.classList.add("show-test");
+//     } else {
+//       footer.classList.remove("show-footer");
+//       divE.classList.remove("show-test");
+//     }
+//   };
+// });
--- functions/pages/index.js
+++ functions/pages/index.js
@@ -14,3 +14,19 @@ export function handleSectionScroll() {
     }
   });
 }
+
+// let sections = document.querySelectorAll("section");
+
+// window.onscroll = function () {
+//   sections.forEach((sec) => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 250;
+//     let height = sec.offsetHeight;
+
+//     if (top >= offset && top < offset + height) {
+//       sec.classList.add("show-animateOnScroll");
+//     } else {
+//       sec.classList.remove("show-animateOnScroll");
+//     }
+//   });
+// };
