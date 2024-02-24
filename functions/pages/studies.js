54ddac7163f3330bd795cac69a59fad21bbd75ad
Personalism <edwinhermili@gmail.com>
2024-02-24 22:09:44 +0700

    Updated

--- functions/pages/studies.js
+++ functions/pages/studies.js
@@ -689,3 +689,319 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-1");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-1");
+    dropdown.classList.toggle("expand-1");
+
+    if (dropdown.classList.contains("expand-1")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-2");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-2");
+    dropdown.classList.toggle("expand-2");
+
+    if (dropdown.classList.contains("expand-2")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-3");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-3");
+    dropdown.classList.toggle("expand-3");
+
+    if (dropdown.classList.contains("expand-3")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-4");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-4");
+    dropdown.classList.toggle("expand-4");
+
+    if (dropdown.classList.contains("expand-4")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-5");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-5");
+    dropdown.classList.toggle("expand-5");
+
+    if (dropdown.classList.contains("expand-5")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-6");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-6");
+    dropdown.classList.toggle("expand-6");
+
+    if (dropdown.classList.contains("expand-6")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-7");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-7");
+    dropdown.classList.toggle("expand-7");
+
+    if (dropdown.classList.contains("expand-7")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-8");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-8");
+    dropdown.classList.toggle("expand-8");
+
+    if (dropdown.classList.contains("expand-8")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-9");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-9");
+    dropdown.classList.toggle("expand-9");
+
+    if (dropdown.classList.contains("expand-9")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-10");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-10");
+    dropdown.classList.toggle("expand-10");
+
+    if (dropdown.classList.contains("expand-10")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-11");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-11");
+    dropdown.classList.toggle("expand-11");
+
+    if (dropdown.classList.contains("expand-11")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-12");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-12");
+    dropdown.classList.toggle("expand-12");
+
+    if (dropdown.classList.contains("expand-12")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-13");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-13");
+    dropdown.classList.toggle("expand-13");
+
+    if (dropdown.classList.contains("expand-13")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-14");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-14");
+    dropdown.classList.toggle("expand-14");
+
+    if (dropdown.classList.contains("expand-14")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("description3-15");
+
+  dropdown.addEventListener("click", function () {
+    let content = document.getElementById("content3-15");
+    dropdown.classList.toggle("expand-15");
+
+    if (dropdown.classList.contains("expand-15")) {
+      content.style.animation = "fadeIn 0.4s";
+      content.style.display = "grid";
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 100);
+    } else {
+      content.style.opacity = "0";
+      setTimeout(() => {
+        content.style.display = "none";
+      }, 400);
+    }
+  });
+});
